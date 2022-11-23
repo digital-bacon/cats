a3688016cd2fdc3209c7ddc54cd0fdf44056ce72
Mark Bieganek <mark.bieganek@telus.com>
2022-11-22 22:21:07 -0700

    Change unit testing to Mocha + Chai assertions

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,12 +2,10 @@ const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, action) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    if (!error) {
       action(data);
-    }
   });
 };
 
 const returnData = (data) => console.log(data);
 
-breedDetailsFromFile('Bombay', returnData)
+module.exports = breedDetailsFromFile;
